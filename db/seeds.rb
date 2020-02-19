68b1104d20ce6038fa7a52146a6ad042e12b64cb
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-19 14:33:01 -0300

    Create seed to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant = Restaurant.create([
+  {name: "MacDonald", address: "Rua Mourato", phone_number: "1199691234", category: "chinese"},
+  {name: "Burger King", address: "Rua de baixo", phone_number: "1199694321", category: "italian"},
+  {name: "Maria da Vila", address: "Na esquina", phone_number: "1199695678", category: "japanese"},
+  {name: "KFC", address: "Rua da direita", phone_number: "1199698765", category: "french"},
+  {name: "Vintão", address: "No pé da ladeira", phone_number: "1199690987", category: "belgian"}])
