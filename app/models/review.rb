c1049bf9ed4aed82f7dfad0cec7234b9d71de387
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-19 14:10:15 -0300

    create database and controller

---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ApplicationRecord
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :content, presence: true
+  belongs_to :restaurant
+end
