3c36904c1d05cf15429d1a570ff7f7e649aaa5e2
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-19 14:58:48 -0300

    add index - controller and view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,17 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
 end
