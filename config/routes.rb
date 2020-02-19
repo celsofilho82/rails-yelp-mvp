f365b672fd619ee8f11f9d94f053eecb950561e5
Celso Ribeiro <celsofilho82@gmail.com>
2020-02-19 17:55:24 -0300

    add review create, index and show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [ :index, :new, :create ]
   end
-  # resources :reviews, only: [ :show, :edit, :update, :destroy ]
-
 
 end
